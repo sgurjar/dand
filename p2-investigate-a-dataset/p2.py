692262a317aec2eaee471e71ff361741d2b743b4
Satyendra Gurjar <sgurjar@gmail.com>
2015-11-30 17:41:17 -0500

    created

---
+++ p2-investigate-a-dataset/p2.py
@@ -0,0 +1,221 @@
+import pandas
+import pandasql
+import datetime
+import csv
+import os.path
+
+def num_rainy_days(filename):
+    weather_data = pandas.read_csv(filename)
+    q = """select count(rain) from weather_data where rain=1"""
+    rainy_days = pandasql.sqldf(q.lower(), locals())
+    return rainy_days
+
+def max_temp_aggregate_by_fog(filename):
+    weather_data = pandas.read_csv(filename)
+    q = """select fog, max(maxtempi) from weather_data group by fog"""
+    foggy_days = pandasql.sqldf(q.lower(), locals())
+    return foggy_days
+
+def avg_weekend_temperature(filename):
+    weather_data = pandas.read_csv(filename)
+    q = """select avg(cast (meantempi as integer)) from weather_data where cast(strftime('%w', date) as integer) in (0, 6)"""
+    mean_temp_weekends = pandasql.sqldf(q.lower(), locals())
+    return mean_temp_weekends
+
+def avg_min_temperature(filename):
+    weather_data = pandas.read_csv(filename)
+    q = """select avg(mintempi) from weather_data where rain=1 and mintempi > 55"""
+    avg_min_temp_rainy = pandasql.sqldf(q.lower(), locals())
+    return avg_min_temp_rainy
+
+def fix_turnstile_data(filenames):
+    for name in filenames:
+        outputfilename = 'updated_' + os.path.basename(name)
+        with open(outputfilename,'wb') as outputfile:
+            output = csv.writer(outputfile)
+            with open(name, 'rb') as f:
+                f = csv.reader(f,skipinitialspace=True)
+                for line in f:
+                    # first 3 columns are same
+                    part1 = line[:3]
+                    rest = line[3:]
+                    # next 5 columns
+                    #   05-06-11,17:00:00,REGULAR,000005554,000000013
+                    assert len(rest)%5 == 0
+                    for i in range(0,len(rest),5):
+                        output.writerow(part1 + rest[i:i+5])
+
+
+def create_master_turnstile_file(filenames, output_file):
+    '''
+    Write a function that takes the files in the list filenames, which all have the
+    columns 'C/A, UNIT, SCP, DATEn, TIMEn, DESCn, ENTRIESn, EXITSn', and consolidates
+    them into one file located at output_file.  There should be ONE row with the column
+    headers, located at the top of the file. The input files do not have column header
+    rows of their own.
+
+    For example, if file_1 has:
+    line 1 ...
+    line 2 ...
+
+    and another file, file_2 has:
+    line 3 ...
+    line 4 ...
+    line 5 ...
+
+    We need to combine file_1 and file_2 into a master_file like below:
+     'C/A, UNIT, SCP, DATEn, TIMEn, DESCn, ENTRIESn, EXITSn'
+    line 1 ...
+    line 2 ...
+    line 3 ...
+    line 4 ...
+    line 5 ...
+    '''
+    with open(output_file, 'w') as master_file:
+        master_file.write('C/A,UNIT,SCP,DATEn,TIMEn,DESCn,ENTRIESn,EXITSn\n')
+        for filename in filenames:
+            with open(filename) as f:
+                for line in f:
+                    master_file.write(line)
+
+
+def filter_by_regular(filename):
+    '''
+    This function should read the csv file located at filename into a pandas dataframe,
+    and filter the dataframe to only rows where the 'DESCn' column has the value 'REGULAR'.
+
+    For example, if the pandas dataframe is as follows:
+    ,C/A,UNIT,SCP,DATEn,TIMEn,DESCn,ENTRIESn,EXITSn
+    0,A002,R051,02-00-00,05-01-11,00:00:00,REGULAR,3144312,1088151
+    1,A002,R051,02-00-00,05-01-11,04:00:00,DOOR,3144335,1088159
+    2,A002,R051,02-00-00,05-01-11,08:00:00,REGULAR,3144353,1088177
+    3,A002,R051,02-00-00,05-01-11,12:00:00,DOOR,3144424,1088231
+
+    The dataframe will look like below after filtering to only rows where DESCn column
+    has the value 'REGULAR':
+    0,A002,R051,02-00-00,05-01-11,00:00:00,REGULAR,3144312,1088151
+    2,A002,R051,02-00-00,05-01-11,08:00:00,REGULAR,3144353,1088177
+    '''
+
+    turnstile_data = pandas.read_csv(filename)
+    turnstile_data = turnstile_data[turnstile_data.DESCn=='REGULAR']
+    return turnstile_data
+
+def get_hourly_entries(df):
+    '''
+    The data in the MTA Subway Turnstile data reports on the cumulative
+    number of entries and exits per row.  Assume that you have a dataframe
+    called df that contains only the rows for a particular turnstile machine
+    (i.e., unique SCP, C/A, and UNIT).  This function should change
+    these cumulative entry numbers to a count of entries since the last reading
+    (i.e., entries since the last row in the dataframe).
+
+    More specifically, you want to do two things:
+       1) Create a new column called ENTRIESn_hourly
+       2) Assign to the column the difference between ENTRIESn of the current row
+          and the previous row. If there is any NaN, fill/replace it with 1.
+
+    You may find the pandas functions shift() and fillna() to be helpful in this exercise.
+
+    Examples of what your dataframe should look like at the end of this exercise:
+
+           C/A  UNIT       SCP     DATEn     TIMEn    DESCn  ENTRIESn    EXITSn  ENTRIESn_hourly
+    0     A002  R051  02-00-00  05-01-11  00:00:00  REGULAR   3144312   1088151                1
+    1     A002  R051  02-00-00  05-01-11  04:00:00  REGULAR   3144335   1088159               23
+    2     A002  R051  02-00-00  05-01-11  08:00:00  REGULAR   3144353   1088177               18
+    3     A002  R051  02-00-00  05-01-11  12:00:00  REGULAR   3144424   1088231               71
+    4     A002  R051  02-00-00  05-01-11  16:00:00  REGULAR   3144594   1088275              170
+    5     A002  R051  02-00-00  05-01-11  20:00:00  REGULAR   3144808   1088317              214
+    6     A002  R051  02-00-00  05-02-11  00:00:00  REGULAR   3144895   1088328               87
+    7     A002  R051  02-00-00  05-02-11  04:00:00  REGULAR   3144905   1088331               10
+    8     A002  R051  02-00-00  05-02-11  08:00:00  REGULAR   3144941   1088420               36
+    9     A002  R051  02-00-00  05-02-11  12:00:00  REGULAR   3145094   1088753              153
+    10    A002  R051  02-00-00  05-02-11  16:00:00  REGULAR   3145337   1088823              243
+    ...
+    ...
+
+    '''
+    df['ENTRIESn_hourly'] = df.ENTRIESn - df.ENTRIESn.shift(1)
+    df.ENTRIESn_hourly.fillna(1, inplace=True)
+    return df
+
+def get_hourly_exits(df):
+    '''
+    The data in the MTA Subway Turnstile data reports on the cumulative
+    number of entries and exits per row.  Assume that you have a dataframe
+    called df that contains only the rows for a particular turnstile machine
+    (i.e., unique SCP, C/A, and UNIT).  This function should change
+    these cumulative exit numbers to a count of exits since the last reading
+    (i.e., exits since the last row in the dataframe).
+
+    More specifically, you want to do two things:
+       1) Create a new column called EXITSn_hourly
+       2) Assign to the column the difference between EXITSn of the current row
+          and the previous row. If there is any NaN, fill/replace it with 0.
+
+    You may find the pandas functions shift() and fillna() to be helpful in this exercise.
+
+    Example dataframe below:
+
+          Unnamed: 0   C/A  UNIT       SCP     DATEn     TIMEn    DESCn  ENTRIESn    EXITSn  ENTRIESn_hourly  EXITSn_hourly
+    0              0  A002  R051  02-00-00  05-01-11  00:00:00  REGULAR   3144312   1088151                0              0
+    1              1  A002  R051  02-00-00  05-01-11  04:00:00  REGULAR   3144335   1088159               23              8
+    2              2  A002  R051  02-00-00  05-01-11  08:00:00  REGULAR   3144353   1088177               18             18
+    3              3  A002  R051  02-00-00  05-01-11  12:00:00  REGULAR   3144424   1088231               71             54
+    4              4  A002  R051  02-00-00  05-01-11  16:00:00  REGULAR   3144594   1088275              170             44
+    5              5  A002  R051  02-00-00  05-01-11  20:00:00  REGULAR   3144808   1088317              214             42
+    6              6  A002  R051  02-00-00  05-02-11  00:00:00  REGULAR   3144895   1088328               87             11
+    7              7  A002  R051  02-00-00  05-02-11  04:00:00  REGULAR   3144905   1088331               10              3
+    8              8  A002  R051  02-00-00  05-02-11  08:00:00  REGULAR   3144941   1088420               36             89
+    9              9  A002  R051  02-00-00  05-02-11  12:00:00  REGULAR   3145094   1088753              153            333
+    '''
+
+    df['EXITSn_hourly'] = df.EXITSn - df.EXITSn.shift(1)
+    df.EXITSn_hourly.fillna(0, inplace=True)
+    return df
+
+def time_to_hour(time):
+    '''
+    Given an input variable time that represents time in the format of:
+    "00:00:00" (hour:minutes:seconds)
+
+    Write a function to extract the hour part from the input variable time
+    and return it as an integer. For example:
+        1) if hour is 00, your code should return 0
+        2) if hour is 01, your code should return 1
+        3) if hour is 21, your code should return 21
+
+    Please return hour as an integer.
+    '''
+
+    hour = int(time[:2])
+    return hour
+
+def reformat_subway_dates(date):
+    '''
+    The dates in our subway data are formatted in the format month-day-year.
+    The dates in our weather underground data are formatted year-month-day.
+
+    In order to join these two data sets together, we'll want the dates formatted
+    the same way.  Write a function that takes as its input a date in the MTA Subway
+    data format, and returns a date in the weather underground format.
+
+    Hint:
+    There are a couple of useful functions in the datetime library that will
+    help on this assignment, called strptime and strftime.
+    More info can be seen here and further in the documentation section:
+    http://docs.python.org/2/library/datetime.html#datetime.datetime.strptime
+    '''
+
+    date_formatted = datetime.datetime.strptime(date,'%m-%d-%y').strftime('%Y-%m-%d')
+    return date_formatted
+
+#filename='../datasets/weather_underground.csv'
+filename='../datasets/turnstile_110507.txt'
+#print num_rainy_days(filename)
+#print max_temp_aggregate_by_fog(filename)
+#print avg_weekend_temperature(filename)
+#print avg_min_temperature(filename)
+#fix_turnstile_data((filename,))
+#create_master_turnstile_file(('updated_turnstile_110507.txt',),'master_turnstile_110507.txt')
+print filter_by_regular('master_turnstile_110507.txt')
